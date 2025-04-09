8019e08ee265cfcd1849ff38e17f955e9664f4c9
karinawinn <156015334+karinawinn@users.noreply.github.com>
2025-04-08 21:05:12 -0600

    Delete Portfolio App/forms.py

--- Portfolio App/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import SubmitField
-
-class PhysicsButton(FlaskForm):
-    phy = SubmitField('Physics')
-
-class ProgrammingButton(FlaskForm):
-    pro = SubmitField('Programming')
